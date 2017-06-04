269e88eae1910805ab54adfb153236b7ae9a3a53
PukaPy <pukalordu@yahoo.com>
2017-06-04 14:40:00 +0300

    Update move_desk.py

--- move_desk.py
+++ move_desk.py
@@ -8,7 +8,11 @@ def run(argv):
 	user = raw_input('Input your user:')
 	desktop = 'C:\\Users\\{}\\Desktop'.format(user)
 	public_desktop = 'C:\\Users\\Public\\Desktop'
-	new_dir = 'C:\\Users\\{}\\Desktop\\{}--{}'.format(user, pukast, str(random.randint(0,1024)))
+	new_dir = 'C:\\Users\\{}\\Desktop\\{}--{}'.format(
+		user,
+		pukast,
+		str(random.randint(0,1024))
+	)
 	if not os.path.isdir(desktop):
 		print 'Specified path not found. ',
 		print 'Path is \'{}\''.format(desktop)
@@ -20,8 +24,16 @@ def run(argv):
 		print "New directory with name {} was made.".format(new_dir)
 	print '-------------------'
 	print sys.argv[2:]
-	move_files(all_files_name, desktop, new_dir)
-	move_files(all_lnk_name, public_desktop, new_dir)
+	move_files(
+		all_files_name,
+		desktop,
+		new_dir
+	)
+	move_files(
+		all_lnk_name,
+		public_desktop,
+		new_dir
+	)
 	print "Done."
 
 def move_files(x, y, z):
@@ -35,8 +47,10 @@ def move_files(x, y, z):
 		if p <= len(x):
 			if ((x[p] not in sys.argv[2:]) and
 				(x[p] != os.path.basename(__file__))):
-				os.rename("{}\\{}".format(y, x[p]),
-						"{}\\{}".format(z, x[p]))
+				os.rename(
+					"{}\\{}".format(y, x[p]),
+					"{}\\{}".format(z, x[p])
+				)
 			else:
 				pass
 			p += 1
